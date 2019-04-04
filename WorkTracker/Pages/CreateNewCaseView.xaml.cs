c9c68a38696b6ca3bb052a17752cddcaf7a66ea0
Alexander Kelman <alexanderkelman95@hotmail.com>
2019-04-04 21:48:04 +0200

    Youtrack api setup

--- WorkTracker/Pages/CreateNewCaseView.xaml.cs
+++ WorkTracker/Pages/CreateNewCaseView.xaml.cs
@@ -1,28 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Threading;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WorkTracker.Pages
 {
-    /// <summary>
-    /// Interaction logic for CreateNewCaseView.xaml
-    /// </summary>
     public partial class CreateNewCaseView : Page
     {
         public CreateNewCaseView()
         {
             InitializeComponent();
+
+            Thread youtrackApi = new Thread(ApiThread);
+            youtrackApi.Start();
+
+        }
+
+        public void ApiThread()
+        {
+            var youtrack = new Youtrack.Youtrack();
+
+            youtrack.GetIssues();
         }
     }
 }
---
+++ Youtrack/Youtrack.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using YouTrackSharp;
+
+namespace Youtrack
+{
+    public class Youtrack
+    {
+        BearerTokenConnection connection;
+
+        public Youtrack()
+        {
+            OpenConnection();
+            GetIssues();
+        }
+
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection = new BearerTokenConnection("https://cgicfuskara.myjetbrains.com/youtrack", "perm:YWxleGFuZGVyLmtlbG1hbg==.V29ya1RyYWNrZXI=.B7p4sOpuQ57NcGMkkGWamrHMPz6Stz");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // Log
+                return false;
+            }
+        }
+
+        public void GetIssues()
+        {    
+            var query = "resolved date: {Last week} .. Today ";
+
+            var Issues = connection.CreateIssuesService().GetIssues(query, take: 1000);
+            //var USersfirst = connection.CreateUserManagementService().GetUsers();
+            //var USerssecond = connection.CreateUserManagementService().GetUsers(start: 10);
+            //var Projects = connection.CreateProjectsService().GetAccessibleProjects();
+
+            Task[] tasks =
+            {
+                Issues
+                //USersfirst,
+                //USerssecond,
+                //Projects
+            };
+
+            Task.WaitAll(tasks);
+
+            var test = Issues.Result;
+            //var query = "resolved date: {Last week} .. Today ";
+            //var issues = connection.CreateIssuesService().GetIssues(query, take: 100);
+
+            //Task task = issues;
+            //Task.WaitAll(task);
+            //var test = issues;
+        }
+    }
+}
