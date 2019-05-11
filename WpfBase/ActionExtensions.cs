1b595ab32708a56f786443560f2cffe5c771eb7c
Alexander Kelman <alexanderkelman95@hotmail.com>
2019-05-11 22:04:33 +0200

    Added WpfBase WIP

---
+++ WpfBase/ActionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WpfBase
+{
+    public static class ActionExtensions
+    {
+        public static Action<object> AddErrorHandling(this Action<object> act, Action<Action<object>, Exception> onError)
+        {
+            return (obj) =>
+            {
+                try
+                {
+                    act(obj);
+                }
+                catch (Exception ex)
+                {
+                    onError(act, ex);
+                }
+            };
+        }
+    }
+}
---
+++ WpfBase/ViewModelBase.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace WpfBase
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetProperty<T>(ref T storage, T value, string propertyName)
+        {
+            if (object.Equals(storage, value)) return false;
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+
+            return true;
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
